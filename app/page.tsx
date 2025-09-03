e994c6c26b7c7fab42f6005f553ff6473cabdbcc
UNAINR <munain605@gmail.com>
2025-09-03 20:11:10 +0500

    chagnes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex items-center justify-center min-h-screen '>
+    <div className='flex items-center justify-center min-h-screen  '>
       <TransactionModalComponent/>
     </div>
   )
